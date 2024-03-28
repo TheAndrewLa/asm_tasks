6706e287610574b8f144cce52af01ca970f2aa0a
AndrewLa <andrew.lzrv@gmail.com>
2024-03-28 13:43:54 +0700

    Updated div10mod10 task

--- div10_mod10.asm
+++ div10_mod10.asm
@@ -7,8 +7,12 @@
 .text
 
 main:
-	li a0, 0x80
+	li a0, 0x30
 	call div10
+	call print_hex
+
+	li a0, 0x30
+	call mod10
 	call print_hex
 
 	exit 0
@@ -28,7 +32,7 @@ mul10:
 # unsigned div10(unsigned a0) -> a0;
 div10:
 	# Recursion fallback
-	sltiu t0, a0, 0xA
+	slti t0, a0, 0xA
 	beqz t0, .div10_body
 	li a0, 0x0
 	ret
@@ -36,17 +40,29 @@ div10:
 	.div10_body:
 	push_reg ra
 	push_reg s0
-	
-	# Using this formula:
-	# x/10 = (1/2) * ((x/4) - ((x/2)/10))
+	push_reg s1
+	push_reg s2
 
-	srli s0, a0, 0x2 # Divide by 4
-	srli a0, a0, 0x1 # Divide by 2
+	# Using this formula:
+	# x/10 = ((x/8) - ((x/4)/10))
+
+	mv s1, a0
+
+	srli s0, a0, 0x3 # Divide by 8
+	srli a0, a0, 0x2 # Divide by 4
 	call div10
 
 	sub a0, s0, a0
-	srli a0, a0, 0x1 # Divide by 2
 
+	mv s2, a0
+	
+	call mul10
+
+	sgtu t0, a0, s1
+	sub a0, s2, t0
+
+	pop_reg s2
+	pop_reg s1
 	pop_reg s0
 	pop_reg ra
 	ret
